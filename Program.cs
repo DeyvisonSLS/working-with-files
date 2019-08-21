4ee6ce6c3aa1ef05110637fc0bde7514e6d816cb
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-08-21 19:38:39 -0300

    Testing using blocks with stream reader and file class

--- Program.cs
+++ Program.cs
@@ -12,17 +12,14 @@ namespace working_with_files
 
             try
             {
-                using(FileStream fs = new FileStream(path, FileMode.Open))
+                using(StreamReader sr = File.OpenText(path))
                 {
-                    using(StreamReader sr = new StreamReader(fs))
+                    while(!sr.EndOfStream)
                     {
-                        while(!sr.EndOfStream)
-                        {
-                            string line = sr.ReadLine();
-                            Console.WriteLine(line);
-                        }
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
                     }
-                }
+                    }
             }
             catch(IOException e)
             {
