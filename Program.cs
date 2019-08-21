2bf0edccbdd2a9dffd887aa29599fbb1eb18b00e
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-08-21 18:07:06 -0300

    Learning the FIle and FIleInfo classes.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace working_with_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"../../../../private/tmp/file1.txt";
+            string targetPath = @"../../../../private/tmp/file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred: " + e.Message);
+            }
+        }
+    }
+}
